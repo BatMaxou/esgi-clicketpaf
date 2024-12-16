ba1edad4a4b48df49cd11812c457943670c0a8db
BatMaxou <maximebatista.lycee@gmail.com>
2024-12-16 18:05:37 +0100

    :art: Put footer into main layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,33 +1,38 @@
+import Head from "next/head";
+
 import "./globals.css";
 
 export const metadata = {
   title: "Click & Paf - Les Solutions qui Claquent",
-  description:
-    "Des solutions rapides et efficaces pour vos problèmes quotidiens. Click & Paf : un, clic et c'est réglé !",
-  keywords:
-    "click, paf, solutions, clic, réglé, problèmes, quotidiens, click & paf, click and paf, click&paf, clickandpaf, solution, seul clic, bricolage, quotidien, rapide, efficace, service, express, assistance, domicile, SOS, sos, administratif, astuces, astuce, pratique",
+  description: "Des solutions rapides et efficaces pour vos problèmes quotidiens. Click & Paf : un, clic et c'est réglé !",
+  keywords: "click, paf, solutions, clic, réglé, problèmes, quotidiens, click & paf, click and paf, click&paf, clickandpaf, solution, seul clic, bricolage, quotidien, rapide, efficace, service, express, assistance, domicile, SOS, sos, administratif, astuces, astuce, pratique",
   "og:image": "/social-image.png",
   "og:image:width": "1200",
   "og:image:height": "630",
   "twitter:card": "summary-large-image",
   "twitter:image": "/social-image.png",
-  "twitter:title": "Click & Paf - Les Solutions qui Claquent",
-  "twitter:description":
-    "Des solutions rapides et efficaces pour vos problèmes quotidiens. Click & Paf : un, clic et c'est réglé !",
+  icon: "/favicon.ico",
+  "apple-touch-icon": "/apple-touch-icon.png",
+  manifest: "/manifest.json",
+  "mask-icon": "/favicon-32x32.svg",
 };
 
 const RootLayout = ({ children }) => {
-  return (
-    <html lang="fr">
-      <header>
-        <link rel="icon" href="/favicon.ico"></link>
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
-        <link rel="manifest" href="/manifest.json"></link>
-        <link rel="mask-icon" href="/favicon-32x32.svg"></link>
-      </header>
-      <body>{children}</body>
-    </html>
-  );
+  return <html lang="fr">
+    <Head>
+      <meta property="og:title" content={metadata.title} />
+      <meta property="og:description" content={metadata.description} />
+      <meta property="twitter:title" content={metadata.title} />
+      <meta property="twitter:description" content={metadata.description} />
+    </Head>
+    <body>
+      {children}
+      
+      <footer>
+        <p>© 2024 Click & Paf - Les Solutions qui Claquent. Tous droits réservés.</p>
+      </footer>  
+    </body>
+  </html>
 };
 
 export default RootLayout;
