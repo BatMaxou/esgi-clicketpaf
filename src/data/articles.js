325a9b36139b1132ec30ff52fa5f0f648386858d
DvK42 <guillaume.couvidou@live.fr>
2025-02-27 18:09:04 +0100

    ✨ Add article

--- src/data/articles.js
+++ src/data/articles.js
@@ -32,6 +32,7 @@ import les5PlusBellesRandonnees from "./articles/les-5-plus-belles-randonnees";
 import cinqPointsNegatifsDeLaConsommationDeTabac from "./articles/5-points-negatifs-de-la-consommation-de-tabac";
 import top5desseriesdefevrier2025 from "./articles/top-5-des-series-de-fevrier-2025";
 import huitConseilsImportantsEnCuisine from "./articles/8-conseils-importants-en-cuisine";
+import les5BossLesPlusDursDeldenRing from "./articles/les-5-boss-les-plus-durs-delden-ring";
 
 export const articles = {
   "5-astuces-pour-proteger-votre-pc": cinqAstucesPourProtegerVotrePC,
@@ -74,4 +75,5 @@ export const articles = {
     cinqPointsNegatifsDeLaConsommationDeTabac,
   "top-5-des-series-de-fevrier-2025": top5desseriesdefevrier2025,
   "8-conseils-importants-en-cuisine": huitConseilsImportantsEnCuisine,
+  "les-5-boss-les-plus-durs-delden-ring": les5BossLesPlusDursDeldenRing,
 };
